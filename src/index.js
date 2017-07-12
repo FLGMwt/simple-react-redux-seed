48e5d18c7095cdb311e5d96cea329943208123f0
Ryan Stelly <ryan.stelly@live.com>
2017-07-11 22:55:27 -0500

    add working redux-react example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 import {createStore} from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+
+document.body.style.fontSize = '50px';
+
+const INCREMENT = 'INCREMENT';
+
+const increment = () => ({type: INCREMENT});
 
 function counter(state = 0, action) {
-  return state + 1;
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    default:
+      return state;
+  }
 }
+
+const RawApp = ({count, onClick}) => (
+  <div>
+    <div>
+      Click count: {count}
+    </div>
+    <button onClick={onClick} style={{fontSize: 50}}>Add One</button>
+  </div>
+);
+
+const App = connect(
+  state => ({count: state}),
+  {onClick: increment}
+)(RawApp);
 
 const store = createStore(counter);
 
-console.log(store.getState());
-
-const App = () => <h1>Yo</h1>;
+const Root = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 ReactDOM.render(
-  <App />,
+  <Root />,
   document.getElementById('app')
 )
